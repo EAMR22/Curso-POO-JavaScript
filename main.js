3f52e507f86ac06e702a83783b177f85bb743bf0
elvishn <elvis1042.rodriguez@gmail.com>
2022-01-12 11:55:57 -0600

    Comenzando a definir prototipos y objetos literales en JS

---
+++ main.js
@@ -0,0 +1,73 @@
+const natalia = { // Es un objeto literal.
+    name: "Natalia",
+    age: 20,
+    cursosAprobados: [
+        "Curso definitivo de HTML y CSS",
+        "Curso práctico de HTML y CSS",
+    ],
+
+    //Métodos:
+    aprobarCurso(nuevoCursito){
+        this.cursosAprobados.push(nuevoCursito); // this hace referencia al objeto natalia
+    },
+}; 
+
+// Hacer que natalia apruebe otro curso:
+
+// natalia.cursosAprobados.push("Curso de Responsive Design");
+
+function Student(name, age, cursosAprobados) { // Es un prototipo
+    this.name = name;
+    this.age = age;
+    this.cursosAprobados = cursosAprobados;
+
+    // Es una forma de crear un método
+    /*aprobarCurso(nuevoCursito){
+        this.cursosAprobados.push(nuevoCursito); // this hace referencia al objeto natalia
+    },*/
+}
+
+// Otra forma de crear un método
+
+Student.prototype.aprobarCurso = function (nuevoCursito){
+    this.cursosAprobados.push(nuevoCursito); // this hace referencia al objeto natalia
+}
+
+// Crear instacias del objeto
+
+const juanita = new Student(
+    "Juanita Alejandra",
+    15,
+    [
+        "Curso de introducción a la producción de videojuegos",
+        "Curso de creación de personajes",
+    ],
+);
+
+// Prototipos con la sintaxis de clases:
+
+class Student2 {
+    constructor({
+        name, 
+        age, 
+        email,
+        cursosAprobados = [],
+    }) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.cursosAprobados = cursosAprobados;
+    }
+
+    // Creando el método
+    aprobarCurso(nuevoCursito){
+        this.cursosAprobados.push(nuevoCursito);
+    }
+};
+
+const miguelito = new Student2({
+    email: "miguelito@platzi.com",
+    name: "Miguelito",
+    age: 28,
+    cursosAprobados: [],
+});
